c988f69bfe7475af75838b1ede38e7f2e5deb40a
Arif Emin Akgül <arifakgl34@gmail.com>
2024-11-11 21:12:22 +0300

    A file was created and a simple calculator code was written.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************************");
+            Console.WriteLine("Basic Calculator(Press 'q' for quit.))");
+            Console.WriteLine("*******************************");
+
+            while (true)
+            {
+                Console.WriteLine("+,-,*,/:");
+
+                string ope = Console.ReadLine();
+
+                if (ope == "q")
+                {
+                    break;
+                }
+                
+                if (ope == "+")
+                {
+                    Console.WriteLine("1.Number:");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.WriteLine("2.Number:");
+                    double y = double.Parse(Console.ReadLine());
+                    
+                    double sum = x + y;
+                    
+                    Console.WriteLine($"{x} + {y} = {sum}");
+                }
+
+                else if (ope == "-")
+                {
+                    Console.WriteLine("1.Number:");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.WriteLine("2.Number:");
+                    double y = double.Parse(Console.ReadLine());
+
+                    double sum = x - y;
+
+                    Console.WriteLine($"{x} - {y} = {sum}");
+                }
+
+                else if (ope == "*")
+                {
+                    Console.WriteLine("1.Number:");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.WriteLine("2.Number:");
+                    double y = double.Parse(Console.ReadLine());
+                    
+                    double sum = x * y;
+                    
+                    Console.WriteLine($"{x} * {y} = {sum}");
+                }
+
+                else if (ope == "/")
+                {
+                    Console.WriteLine("1.Number:");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.WriteLine("2.Number:");
+                    double y = double.Parse(Console.ReadLine());
+                    
+                    double sum = x / y;
+                    
+                    Console.WriteLine($"{x} / {y} = {sum}");
+                }
+
+                else
+                {
+                    Console.WriteLine("İnvalid transaction");
+                }
+            }
+        }
+    }
+}
